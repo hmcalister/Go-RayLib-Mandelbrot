e183714cf6b7e4788085e68a1b28b502e6bcf8c1
Hayden McAlister <haydenmcalister49@gmail.com>
2024-10-25 00:14:55 +1300

    Use new max iterations value from draw parameters in createMandelbrotTexture

--- mandelbrot.go
+++ mandelbrot.go
@@ -3,29 +3,24 @@ package main
 import rl "github.com/gen2brain/raylib-go/raylib"
 
 func createMandelbrotTexture(params drawParameters) rl.Texture2D {
-	maxIterations := 100
-
 	image := rl.GenImageColor(int(WINDOW_WIDTH), int(WINDOW_HEIGHT), rl.Black)
 
 	var pixelX int32
 	var pixelY int32
 	for pixelX = 0; pixelX < WINDOW_WIDTH; pixelX += 1 {
 		for pixelY = 0; pixelY < WINDOW_HEIGHT; pixelY += 1 {
-			pixelComplex := ComplexNumber{
-				params.centerX + (float64(pixelX)-float64(WINDOW_WIDTH)/2)/params.zoom,
-				params.centerY + (float64(pixelY)-float64(WINDOW_HEIGHT)/2)/params.zoom,
-			}
+			pixelComplex := params.convertPixelToComplex(pixelX, pixelY)
 
 			valueComplex := ComplexNumber{0, 0}
 			iterationIndex := 0
 
 			// As soon as the magnitude is above 4, the product will diverge
-			for valueComplex.Product(valueComplex).Magnitude() < 4.0 && iterationIndex < maxIterations {
+			for valueComplex.Product(valueComplex).Magnitude() < 4.0 && iterationIndex < params.maxIterations {
 				valueComplex = valueComplex.Product(valueComplex).Add(pixelComplex)
 				iterationIndex += 1
 			}
 
-			colorLERP := uint8(255 * iterationIndex / maxIterations)
+			colorLERP := uint8(255 * iterationIndex / params.maxIterations)
 			color := rl.NewColor(colorLERP, colorLERP, colorLERP, 255)
 			rl.ImageDrawPixel(image, pixelX, pixelY, color)
 		}
