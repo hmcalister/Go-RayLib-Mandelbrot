44b91e0b52aea5967836d94a20900f6470fd2071
Hayden McAlister <haydenmcalister49@gmail.com>
2024-10-24 21:55:18 +1300

    Add skeleton main method with basic window initalization

---
+++ main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"flag"
+
+	rl "github.com/gen2brain/raylib-go/raylib"
+)
+
+func main() {
+	rlLogLevelFlag := flag.String("rlLogLevel", "none", "Set the raylib log level. Valid values are: fatal, error, warning, info, debug, trace, none.")
+	slogLevelFlag := flag.String("slogLevel", "none", "Set the slog level. Valid values are: fatal, error, warning, info, debug, trace, none.")
+	slogFormatFlag := flag.String("slogFormat", "pretty", "Set the slog format. Valid values are: text, pretty, json.")
+	flag.Parse()
+	setupLogging(*slogLevelFlag, *slogFormatFlag, *rlLogLevelFlag)
+
+	rl.InitWindow(800, 450, "raylib [core] example - basic window")
+	defer rl.CloseWindow()
+
+	rl.SetTargetFPS(60)
+
+	for !rl.WindowShouldClose() {
+		rl.BeginDrawing()
+		rl.ClearBackground(rl.RayWhite)
+
+		rl.DrawText("Congrats! You created your first window!", 190, 200, 20, rl.LightGray)
+
+		rl.EndDrawing()
+	}
+}
